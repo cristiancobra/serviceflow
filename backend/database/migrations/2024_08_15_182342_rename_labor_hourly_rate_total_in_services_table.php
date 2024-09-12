f3eab38a7ddef5374881110f3b07a7c42a779306
Cristian Cobra <cristiancobra13@gmail.com>
2024-09-12 19:10:41 -0300

    again

--- backend/database/migrations/2024_08_15_182342_rename_labor_hourly_rate_total_in_services_table.php
+++ backend/database/migrations/2024_08_15_182342_rename_labor_hourly_rate_total_in_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('services', function (Blueprint $table) {
-            $table->renameColumn('labor_hourly_total', 'labor_hourly_total');
+            $table->renameColumn('labor_hourly_rate_total', 'labor_hourly_total');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('services', function (Blueprint $table) {
-            $table->renameColumn('labor_hourly_total', 'labor_hourly_total');
+            $table->renameColumn('labor_hourly_total', 'labor_hourly_rate_total');
         });
     }
 };
